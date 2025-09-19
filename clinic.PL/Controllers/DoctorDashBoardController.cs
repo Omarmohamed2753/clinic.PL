ae7a48a2567286de5b38f32f888309e57612be8d
Omar <omarmassoud826@gmail.com>
2025-09-20 00:22:11 +0300

    DoctorDashBoardController

---
+++ clinic.PL/Controllers/DoctorDashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clinic.PL.Controllers
+{
+    public class DoctorDashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
